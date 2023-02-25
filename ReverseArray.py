3eab0470c240924e3cbfba785738154500979574
Gaurav Tak <98230369+gaurav-2-0-0-2@users.noreply.github.com>
2023-02-25 14:43:21 +0530

    Reversing an array using two pointer

---
+++ ReverseArray.py
@@ -0,0 +1,22 @@
+def R_arr(arr):
+    left = 0 
+    right = len(arr)-1
+   
+
+    while(left<right):
+        temp = arr[left]
+        arr[left] = arr[right]
+        arr[right] = temp 
+        left += 1
+        right -= 1
+
+    return arr
+
+A = [2,3,4,5,6,7,8]
+print("Reversed array will be:")
+print(R_arr(A))
+
+    
+
+
+
