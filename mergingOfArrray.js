8a30f6623f2e0ccec8b2407db45edd753e408d99
Gaurav Tak <98230369+gaurav-2-0-0-2@users.noreply.github.com>
2023-03-05 20:43:42 +0530

    code for merging of array added

--- mergingOfArrray.js
+++ mergingOfArrray.js
@@ -1,24 +1,31 @@
-
+// Merging two sorted arrays 
 function mergingOfArray(A,B,C){
     let i = 0;
     let j = 0;
     let k = 0;
 
     while(i < A.length && j < B.length){
-            if (A[i] < B[j]) {
+            if (A[i] < B[j]) {  // comparing the values of arrays, smaller value gets copied in the C 
                 C[k++] = A[i++];
-                console.log(A);
             }else{
                 C[k++] = B[j++];
-                console 
+                
             }
-    }
+        }
+        // copy the remaining elements 
+            while(i < A.length){
+                C[k++] = A[i++]
+            }
+            while(j < B.length){
+                C[k++] = B[j++]
+            }
+   
     return C;
 }
 
 
-const A = [1,2,3,4,5];
-const B = [6,7,8,9,10];
+const A = [1,3,11,121,125];
+const B = [2,5,9,10,12];
 const C = [];
 
 const result = mergingOfArray(A,B,C);
